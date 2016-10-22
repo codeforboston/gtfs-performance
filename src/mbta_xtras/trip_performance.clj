6280aa7ad3c3ba1ae6e1482733ceb53f24faf673
Brian Sanders <bdougsand+cfb@gmail.com>
2016-10-22 22:22:25 +0000

    Calculate on-time performance for a given trip-id and start date

--- src/mbta_xtras/trip_performance.clj
+++ src/mbta_xtras/trip_performance.clj
@@ -6,9 +6,10 @@
             [monger
              [collection :as mc]
              [core :as mg]])
-  (:import [java.time LocalDate LocalDateTime LocalTime Instant]
+  (:import [java.time LocalDate LocalDateTime LocalTime Instant ZonedDateTime ZoneId]
            [java.time.format DateTimeFormatter]
            java.time.temporal.ChronoUnit))
+
 
 (defn prep-collection [db mongo-coll]
   (mc/ensure-index db mongo-coll
@@ -26,8 +27,14 @@
 
 
 (def datetime-format (DateTimeFormatter/ofPattern "yyyyMMdd"))
-(defn datetime-for-str [date-str] (LocalDateTime/of (LocalDate/parse date-str datetime-format)
-                    LocalTime/MIDNIGHT))
+(defn datetime-for-str
+  ([date-str tz]
+   (-> date-str
+       (LocalDate/parse datetime-format)
+       (LocalDateTime/of LocalTime/MIDNIGHT)
+       (ZonedDateTime/of (ZoneId/of tz))))
+  ([date-str]
+   (datetime-for-str date-str "America/New_York")))
 
 (defn set-hours
   [dt h]
@@ -48,13 +55,48 @@
   the format hh:mm:ss, which is roughly the wall clock time, but it can be
   bigger than 23 for trips that begin on one day and end the next day."
   [ref-date time-str]
-  (let [[h m s] (re-find #"(\d\d?):(\d\d):(\d\d)" time-str)]
+  (let [[_ h m s] (re-find #"(\d\d?):(\d\d):(\d\d)" time-str)]
     (-> ref-date
         (.truncatedTo ChronoUnit/DAYS)
-        (set-hours (long h))
-        (.withMinute (long m))
-        (.withSecond (long s)))))
+        (set-hours (Long/parseLong h))
+        (.withMinute (Long/parseLong m))
+        (.withSecond (Long/parseLong s)))))
 
+(defn trip-ids-for-date
+  ([db date-str]
+   (into #{} (map :trip-id)
+         (mc/find-maps db "trip-stops" {:trip-start date-str})))
+  ([db]
+   (trip-ids-for-date db (.format datetime-format (LocalDateTime/now)))))
+
+(defn- transduce-stop-times
+  [start-date]
+  (let [ref-date (datetime-for-str start-date)]
+    (map (fn [stop-time]
+           [(Integer/parseInt (:stop-sequence stop-time))
+            (->> (:arrival-time stop-time)
+                 (offset-time ref-date)
+                 (.toInstant)
+                 (.getEpochSecond))]))))
+
+(defn scheduled-arrivals [db trip-id start-date]
+  (into (sorted-map)
+        (transduce-stop-times start-date)
+        (mc/find-maps db "stop-times" {:trip-id trip-id})))
+
+(defn trip-performance
+  ""
+  [db trip-id start-date]
+  (let [scheduled (scheduled-arrivals db trip-id start-date)]
+    (->> (mc/find-maps db "trip-stops" {:trip-id trip-id,
+                                        :trip-start start-date})
+         (keep (fn [stop]
+                 (when-let [sched (get scheduled (:stop-sequence stop))]
+                   (merge
+                    (dissoc stop :_id)
+                    {:delay (- (:arrival-time stop) sched)
+                     :scheduled-arrival sched}))))
+         (sort-by :stop-sequence))))
 
 (defn read-updates-into-db
   "Pull trip stop updates continually and insert them into the given Mongo
