8034120c6e7e7837a703aa9253eafdbf4c02df85
Brian Sanders <bdougsand+cfb@gmail.com>
2016-10-27 17:37:10 +0000

    Cleanup of the handler namespace, API stubs
    - Created several stubs for the trip performance APIs I'd ultimately
      like to implement.

--- src/mbta_xtras/api_spec.clj
+++ src/mbta_xtras/api_spec.clj
@@ -1,10 +1,16 @@
 (ns mbta-xtras.api-spec
-  (:require [clojure.spec :as s]))
+  (:require [clojure.spec :as s]
+            [clojure.string :as str]
+            [clojure.data.json :as json]))
 
+(s/def ::positive-int-str #(re-matches #"\d+"))
 (s/def ::float-str #(re-matches #"-?\d+(\.\d+)" %))
 (s/def ::q string?)
 (s/def ::lat (s/and ::float-str #(<= -90 (Float/parseFloat %) 90)))
 (s/def ::lon (s/and ::float-str #(<= -180 (Float/parseFloat %) 190)))
+(s/def ::direction ::positive-int-str)
+(s/def ::route-id string?)
+(s/def ::route string?)
 (s/def ::stop-id string?)
 (s/def ::stop-sequence pos-int?)
 (s/def ::trip-id string?)
@@ -31,8 +37,62 @@
 (s/def ::travel-times-request
   (s/keys :req-un [::from-datetime ::to-datetime ::from-stop ::to-stop]))
 
+(s/def ::dwells-request
+  (s/keys :req-un [::from-datetime ::to-datetime ::stop-id]
+          :opt-un [::route ::direction]))
+
 (s/def ::trips-for-stop-request
   (s/keys :req-un [::stop-id]))
 
 (s/def ::trip-updates-request
   (s/keys ::req-un [::trip-id ::trip-start]))
+
+(s/def ::stop ::stop-id)
+(s/def ::route ::route-id)
+(s/def ::headways-request
+  (s/keys ::req-un [::stop ::from-datetime ::to-datetime]
+          ::opt-un [::to-stop ::route]))
+
+
+;; Utilities:
+(defn explain-pred [pred]
+  (case (first pred)
+    contains? (str "missing parameter: " (name (last pred)))
+
+    (or and) (str/join (str " " (str/upper-case (str (first pred))) " ")
+                       (map #(str "(" (explain-pred %) ")") (rest pred)))
+
+    (re-matches re-find) (str "doesn't match pattern: "
+                              (second pred))
+    (str "doesn't match predicate: " pred)))
+
+(defn explain-problem [{:keys [path pred]}]
+  (str (when path
+         (str (str/join "." (map name path)) " "))
+
+       (explain-pred pred)))
+
+(defn error-message [expl]
+  (map explain-problem (::s/problems expl)))
+
+(defn wrap-json [x]
+  (cond
+    (:status x) x
+
+    :else
+    {:status 200
+     :headers {"Content-type" "application/json"}
+     :body (if (string? x) x (json/write-str x))}))
+
+(defn api-endpoint [spec f]
+  (fn [{:keys [params] :as req}]
+    (if-let [expl (s/explain-data spec params)]
+      {:status 400
+       :headers {"Content-type" "application/json"}
+       :body (json/write-str {:errors (error-message expl)
+                              :request-params params})}
+
+      (wrap-json (f req)))))
+
+(defmacro defapi [name spec params & body]
+  `(def ~name (api-endpoint ~spec (fn ~params ~@body))))
--- src/mbta_xtras/handler.clj
+++ src/mbta_xtras/handler.clj
@@ -3,65 +3,23 @@
             [compojure.core :refer [context defroutes GET POST]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.params :refer [wrap-params]]
-            [mbta-xtras.api-spec :as api]
+            [mbta-xtras.api-spec :as api :refer [defapi]]
             [mbta-xtras.db :as db]
             [mbta-xtras.trip-performance :as trip]
             [clojure.spec :as s]
             [clojure.string :as str]))
 
 
-(defn explain-pred [pred]
-  (case (first pred)
-    contains? (str "missing parameter: " (name (last pred)))
-
-    (or and) (str/join (str " " (str/upper-case (str (first pred))) " ")
-                       (map #(str "(" (explain-pred %) ")") (rest pred)))
-
-    (re-matches re-find) (str "doesn't match pattern: "
-                              (second pred))
-    (str "doesn't match predicate: " pred)))
-
-(defn explain-problem [{:keys [path pred]}]
-  (str (when path
-         (str (str/join "." (map name path)) " "))
-
-       (explain-pred pred)))
-
-(defn error-message [expl]
-  (map explain-problem (::s/problems expl)))
-
-(defn api-endpoint [spec f]
-  (fn [{:keys [params] :as req}]
-    (if-let [expl (s/explain-data spec params)]
-      {:status 400
-       :headers {"Content-type" "application/json"}
-       :body (json/write-str {:errors (error-message expl)
-                              :request-params params})}
-
-      (f req))))
-
-(defn wrap-json [x]
-  (if (map? x)
-    x
-    {:status 200
-     :headers {"Content-type" "application/json"}
-     :body x}))
-
-(defmacro defapi [name spec params & body]
-  `(def ~name (api-endpoint ~spec (fn ~params
-                                    (wrap-json ~@body)))))
-
 (defapi find-stops ::api/find-stops-query
   [{:keys [db params]}]
-  (json/write-str (db/find-stops db params)))
+  (db/find-stops db params))
 
 (defapi trip-performance ::api/trip-performance-request
   [{:keys [db params]}]
-  (json/write-str
-   (let [results (trip/trip-performance db
-                                        (:trip-id params)
-                                        (:trip-start params))]
-     {:performance results})))
+  (let [results (trip/observed-trip-performance db
+                                                (:trip-id params)
+                                                (:trip-start params))]
+    {:performance results}))
 
 (defapi travel-times ::api/travel-times-request
   [{:keys [db params]}]
@@ -71,8 +29,19 @@
                        (Long/parseLong from-datetime)
                        (Long/parseLong to-datetime))))
 
+(defapi dwells ::api/dwells-request
+  [{:keys [db params]}]
+  {:status 501
+   :body "Not implemented"})
+
+(defapi headways ::api/headways-request
+  [{:keys [db params]}]
+  {:status 501
+   :body "Not implemented"})
+
 (defapi trips-for-stop ::api/trips-for-stop-request
   [{:keys [db params]}]
+
   (db/find-trips-for-stop db (:stop-id params)))
 
 (defapi trip-updates ::api/trip-updates-request
@@ -84,9 +53,13 @@
 (defroutes handler
   (context "/xapi" []
            (GET "/find_stops" []  find-stops)
-           (GET "/trip_performance" []  trip-performance)
-           (GET "/travel_times" [] travel-times)
            (GET "/trips_for_stop" [] trips-for-stop)
+           (GET "/trip_performance" []  trip-performance)
+
+           ;; MBTA Performance API:
+           (GET "/dwells" [] dwells)
+           (GET "/headways" [] headways)
+           (GET "/traveltimes" [] travel-times)
            (GET "/trip_updates" [] trip-updates)))
 
 (def app
